37c8837571dbb9dca9f91a8becff7d059fe86075
faneezali <faneezzulfiqarali@gmail.com>
2026-02-17 00:13:33 +0500

    Add Rectangle class extending Shape

---
+++ app/src/main/java/com/example/debugz/Rectangle.java
@@ -0,0 +1,13 @@
+package com.example.debugz;  // Use your actual package name
+
+public class Rectangle extends Shape {
+    private int width;
+    private int height;
+
+    public Rectangle(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
